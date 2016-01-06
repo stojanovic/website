87193ece7a0371e0602bfc441256046fc9d49eb7
Slobodan Stojanovic <slobodan@cloudhorizon.com>
2016-01-06 18:50:26 +0100

    Refactor scripts and use ssl all the time.

--- js/app.js
+++ js/app.js
@@ -1,39 +1,38 @@
+'use strict'
+
 var request        = new XMLHttpRequest()
-var url            = 'https://random-art.herokuapp.com/'
-var background     = document.getElementsByClassName('background')[0]
-var artTitle       = document.getElementsByClassName('art-title')[0]
-var artCreator     = document.getElementsByClassName('art-creator')[0]
-var artDescription = document.getElementsByClassName('art-description')[0]
-var artLocation    = document.getElementsByClassName('art-location')[0]
-var artImage       = document.getElementsByClassName('art-image')[0]
-var artLink        = document.getElementsByClassName('art-link')[0]
+  , url            = 'https://random-art.herokuapp.com/'
+  , background     = document.getElementById('background')
+  , artTitle       = document.getElementById('art-title')
+  , artCreator     = document.getElementById('art-creator')
+  , artDescription = document.getElementById('art-description')
+  , artLocation    = document.getElementById('art-location')
+  , artImage       = document.getElementById('art-image')
+  , artLink        = document.getElementById('art-link')
 
-request.onreadystatechange = function() {
-  if (request.readyState === 4 && request.status === 200) {
-    var art = JSON.parse(request.responseText)
-    console.log('ART', art)
+function process(art) {
+  background.style.backgroundImage = 'url(' + art.image.replace(/^http:/, 'https:') + ')'
+  artTitle.innerHTML = art.title
+  
+  if (art.date)
+    artTitle.innerHTML = artTitle.innerHTML + ' (' + art.date + ')'
+  
+  if (art.creator)
+    artCreator.innerHTML = '<span>Artist:</span> ' + art.creator
+  
+  if (art.description)
+    artDescription.innerHTML = '<span>Description:</span> ' + art.description
+  
+  if (art.location)
+    artLocation.innerHTML = '<span>Location:</span> ' + art.location
 
-    background.style.backgroundImage = 'url(' + art.image + ')'
-
-    artTitle.innerHTML         = art.title
-    
-    if (art.date)
-      artTitle.innerHTML       = artTitle.innerHTML + ' (' + art.date + ')'
-    
-    if (art.creator)
-      artCreator.innerHTML     = '<span>Artist:</span> ' + art.creator
-    
-    if (art.description)
-      artDescription.innerHTML = '<span>Description:</span> ' + art.description
-    
-    if (art.location)
-      artLocation.innerHTML    = '<span>Location:</span> ' + art.location
-
-    artImage.innerHTML         = '<span>Full image:</span> <a href="' + art.image + '" target=_blank>' + art.image + '</a>'
-
-    artLink.innerHTML          = '<span>Source:</span> <a href="' + art.link + '" target=_blank>Google Art Project</a>'
-  }
+  artImage.innerHTML = '<span>Full image:</span> <a href="' + art.image + '" target=_blank>' + art.image + '</a>'
+  artLink.innerHTML = '<span>Source:</span> <a href="' + art.link + '" target=_blank>Google Art Project</a>'
 }
 
+request.onreadystatechange = function() {
+  if (request.readyState === 4 && request.status === 200)
+    process(JSON.parse(request.responseText))
+}
 request.open('GET', url, true)
 request.send()
